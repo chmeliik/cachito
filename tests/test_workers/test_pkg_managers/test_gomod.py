25c1a230106ad09297108085df18b8b06f145f85
Adam Cmiel <acmiel@redhat.com>
2021-03-11 15:33:54 +0100

    Add function that returns path to go subpackage
    Will be reused by other code.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/test_workers/test_pkg_managers/test_gomod.py
+++ tests/test_workers/test_pkg_managers/test_gomod.py
@@ -11,6 +11,7 @@ import pytest
 from cachito.workers.pkg_managers.gomod import (
     get_golang_version,
     resolve_gomod,
+    path_to_subpackage,
     _merge_bundle_dirs,
     _merge_files,
     _parse_name_and_version,
@@ -841,3 +842,17 @@ def test_set_full_local_dep_relpaths_no_match():
 def test_get_allowed_local_deps(mock_worker_config, allowlist, module_name, expect_allowed):
     mock_worker_config.return_value.cachito_gomod_file_deps_allowlist = allowlist
     assert _get_allowed_local_deps(module_name) == expect_allowed
+
+
+@pytest.mark.parametrize(
+    "parent, subpackage, expect_path",
+    [
+        ("github.com/foo", "github.com/foo", ""),
+        ("github.com/foo", "github.com/foo/bar", "bar"),
+        ("github.com/foo", "github.com/foo/bar/baz", "bar/baz"),
+        ("github.com/foo/bar", "github.com/foo/bar/baz", "baz"),
+        ("github.com/foo", "github.com/foo/github.com/foo", "github.com/foo"),
+    ],
+)
+def test_path_from_module_to_package(parent, subpackage, expect_path):
+    assert path_to_subpackage(parent, subpackage) == expect_path
